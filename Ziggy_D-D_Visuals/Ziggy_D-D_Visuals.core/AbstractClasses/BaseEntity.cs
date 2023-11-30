7ccad3f0dd76cafa3fbf76b9ff77f4a450ad833f
De Caluwé Ziggy <71629817+DeCaluweZiggy@users.noreply.github.com>
2023-11-30 22:02:47 +0100

    Add abstract class character

---
+++ Ziggy_D-D_Visuals/Ziggy_D-D_Visuals.core/AbstractClasses/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Ziggy_D_D_Visuals.core.AbstractClasses
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Ziggy_D-D_Visuals/Ziggy_D-D_Visuals.core/AbstractClasses/Character.cs
@@ -0,0 +1,24 @@
+﻿namespace Ziggy_D_D_Visuals.core.AbstractClasses
+{
+    public abstract class Character: BaseEntity
+    {
+        public string NameCharacter  { get; private set; }       
+        public int BaseArmor { get; private set; }
+        public int BaseAttack { get; private set;}
+        public int BaseHealth { get; private set; }
+
+        public Character(string nameCharacter, int baseArmor, int baseAttack, int baseHealth)
+        {
+            NameCharacter = nameCharacter;
+            BaseArmor = baseArmor;
+            BaseAttack = baseAttack;
+            BaseHealth = baseHealth;            
+        }
+
+        public abstract void ReceiveRawDamageWithoutArmor(int rawDamage);
+        public abstract void ReceiveDamageWithArmor(int damage);
+
+        public abstract void Attack(Character characterToAttack);
+
+    }
+}
